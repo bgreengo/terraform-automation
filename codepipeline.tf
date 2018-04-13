930bc23b90f6d7e5ce68d3a36ba5182020ee97a1
Blake Green <green@Blakes-MacBook-Pro.local>
2018-04-13 08:46:33 -0700

    still working on the role permissions

--- codepipeline.tf
+++ codepipeline.tf
@@ -32,9 +32,7 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
     {
       "Effect":"Allow",
       "Action": [
-        "s3:GetObject",
-        "s3:GetObjectVersion",
-        "s3:GetBucketVersioning"
+        "s3:*",
       ],
       "Resource": [
         "${aws_s3_bucket.wordpress.arn}",
